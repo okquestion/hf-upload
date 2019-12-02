b8a166d87404cf292577df41e58553f2b9edc586
lixue <lixue@zcool.com.cn>
2019-12-02 18:07:01 +0800

    clean code

--- build/webpack.config.common.js
+++ build/webpack.config.common.js
@@ -5,19 +5,6 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'babel-loader',
-            options: {
-              cacheDirectory: true
-            }
-          },
-          'eslint-loader'
-        ]
-      },
-      {
         test: /\.tsx?$/,
         use: 'ts-loader',
         exclude: /node_modules/
